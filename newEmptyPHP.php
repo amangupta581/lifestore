4233f4c0aa0098890fef2d74f77942b4d1ccab96
AMAN KUMAR GUPTA <amangupta111999@gmail.com>
2020-08-28 18:24:19 +0530

    This is a Ecommerce project.

--- newEmptyPHP.php
+++
@@ -1,9 +0,0 @@
-<?php
-$connn = mysqli_connect("localhost", "root", "", "ecommerce") or die(mysqli_error($connn));
-$query="select * from users";
-$res = mysqli_query($connn, $query);
-$rows = mysqli_fetch_assoc($res);
- echo $rows['first_name'] ;
-
- 
-        ?>
